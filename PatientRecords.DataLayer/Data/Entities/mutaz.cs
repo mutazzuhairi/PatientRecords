aa273931c0b0c5dc7265e3200542a61a1561efa7
mutazzuhairi <mutazzuhairi@outlook.com>
2021-09-11 10:51:22 +0300

    AutoGenerators

---
+++ PatientRecords.DataLayer/Data/Entities/mutaz.cs
@@ -0,0 +1,24 @@
+﻿using PatientRecords.DataLayer.DataUtilities.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientRecords.DataLayer.Data.Entities
+{
+    public class mutaz
+    {
+        [Required]
+        [StringLength(200)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(256)]
+        public string OfficialId { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        [StringLength(256)]
+        public string Email { get; set; }
+        public ICollection<PatientRecord> PatientRecords { get; set; }
+    }
+}
--- PatientRecords.DataLayer/DataUtilities/DBContext/MainContext.cs
+++ PatientRecords.DataLayer/DataUtilities/DBContext/MainContext.cs
@@ -1,4 +1,4 @@
-﻿using PatientRecords.DataLayer.Data.Entities;
+using PatientRecords.DataLayer.Data.Entities;
 using PatientRecords.DataLayer.Data.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -6,10 +6,10 @@ using PatientRecords.DataLayer.DataUtilities.Extensions;
 
 namespace PatientRecords.DataLayer.DataUtilities.DBContext
 {
+ 
     public sealed class MainContext : IdentityDbContext<User>
     {
         
-
         public MainContext (DbContextOptions<MainContext> options) : base(options)
         {
 
@@ -19,22 +19,23 @@ namespace PatientRecords.DataLayer.DataUtilities.DBContext
 
         }
 
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.AddRestrictToRelationshipOnDelete();
-            modelBuilder.ApplyConfiguration(new PatientConfiguration());
-            modelBuilder.ApplyConfiguration(new PatientRecordConfiguration());
-            modelBuilder.ApplyConfiguration(new UserConfiguration());
-            modelBuilder.ApplyConfiguration(new RoleConfiguration());
-
+            modelBuilder.ApplyConfiguration(new  mutazConfiguration());
+            modelBuilder.ApplyConfiguration(new  PatientConfiguration());
+            modelBuilder.ApplyConfiguration(new  PatientRecordConfiguration());
+            modelBuilder.ApplyConfiguration(new  RoleConfiguration());
+            modelBuilder.ApplyConfiguration(new  UserConfiguration());
             base.OnModelCreating(modelBuilder);
         } 
 
+        public DbSet<mutaz> mutazs { get; set; }
         public DbSet<Patient> Patients { get; set; }
-        public DbSet<PatientRecord > PatientRecords { get; set; }
- 
- 
-
+        public DbSet<PatientRecord> PatientRecords { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+       
     }
 }
+
